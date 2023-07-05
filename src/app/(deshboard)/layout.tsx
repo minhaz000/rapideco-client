b31a4d5de5db6af95c3b86c2a40d0cfd102695fc
rinkuahamed <ringkuali18@gmail.com>
2023-07-05 11:25:46 +0600

    layout

---
+++ src/app/(deshboard)/layout.tsx
@@ -0,0 +1,4 @@
+// import "../../css/globals.css";
+export default function BlueLayout({ children }) {
+  return <div style={{ border: "10px solid blue" }}>{children}</div>;
+}
---
+++ src/app/(deshboard)/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h2>hello</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        mollitia odit hic, non consequuntur delectus incidunt! Doloremque nam
+        pariatur autem?
+      </p>
+    </div>
+  );
+};
+
+export default page;

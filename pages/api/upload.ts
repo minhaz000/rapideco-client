b65063a6f424cb1f6f7d8ee1d7f76804e4f1f803
minhaz000 <mrmminhaz@gmail.com>
2023-07-16 10:21:25 +0600

    Upload file

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,17 +1,41 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from "next-connect";
+import path from "path";
 import multer from "multer";
 const apiRoute = createRouter<NextApiRequest, NextApiResponse>();
+const storage = multer.diskStorage({
+  destination: "./public/uploads",
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now();
+    cb(
+      null,
+      file.originalname.toLocaleLowerCase().split(" ").join("-").split(".")[0] +
+        "-" +
+        uniqueSuffix +
+        path.extname(file.originalname)
+    );
+  },
+});
 const upload: any = multer({
-  storage: multer.diskStorage({
-    destination: "public/uploads",
-    filename: (req: any, file: any, cb) => cb(null, file.originalname),
-  }),
+  storage: storage,
+  // limits: { fieldNameSize: 20 * 1000000 },
+  fileFilter: (req, file, cb) => {
+    const suportedformats = /png|jpeg|jpg|pjp|pjpeg|jfif|svg|webp|/;
+    const extension = path.extname(file.originalname);
+
+    suportedformats.test(extension)
+      ? cb(null, true)
+      : cb(new Error(" invalid file type "));
+  },
 });
 
 apiRoute.use(upload.array("files"));
 apiRoute.post((req: any, res: any) => {
-  res.status(200).json({ data: "Success" });
+  req.files.map((item: any) => {
+    item.img_url = `${req.headers.host}/${item.path.split("public/")[1]}`;
+  });
+
+  res.status(201).json({ data: req.files });
 });
 export default apiRoute.handler({
   onError: (err: any, req, res) => {

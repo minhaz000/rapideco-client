63813471fcdeef5237fb73ce63037ffa18875d7c
rinkuahamed <ringkuali18@gmail.com>
2023-11-07 20:32:06 +0600

    changes

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -5,7 +5,11 @@ import React, { useEffect } from "react";
 async function ClientLayout({ children }: { children: React.ReactNode }) {
   // const data = await axios.get("api/v0/categories");
   // const data = await axios.get("http://localhost:3000/assets/site.settings.json");
+<<<<<<< HEAD
 
+=======
+  const data = "d";
+>>>>>>> 89552644346379f88e1dba58d868b4f32c81ba70
   return (
     <>
       <Header />

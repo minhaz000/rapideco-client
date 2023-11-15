ba366c1cdb2e17cd548f105e2f76c4dbd0fcef82
rinkuahamed <ringkuali18@gmail.com>
2023-11-15 11:09:30 +0600

    updated

--- src/app/(root)/product/page.tsx
+++ src/app/(root)/product/page.tsx
@@ -1,6 +1,10 @@
 import ProductSinglePage from "../../../components/ProductSinglePage/ProductSinglePage";
 const SingleProduct = async () => {
-  return <section className="max-w-screen-xl mx-auto px-3 lg:px-10 mt-6">{<ProductSinglePage />}</section>;
+  return (
+    <section className="max-w-screen-xl mx-auto px-3 lg:px-10 mt-6">
+      {<ProductSinglePage />}
+    </section>
+  );
 };
 
 export default SingleProduct;

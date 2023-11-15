0f760b83889efe45a6f2246735418d1d41d52e07
rinkuahamed <ringkuali18@gmail.com>
2023-11-15 11:12:19 +0600

    update

---
+++ src/app/(root)/product/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProductSinglePage from "../../../components/ProductSinglePage/ProductSinglePage";
+const SingleProduct = async () => {
+  return (
+    <section className="max-w-screen-xl mx-auto px-3 lg:px-10 mt-6">
+      {<ProductSinglePage />}
+    </section>
+  );
+};
+
+export default SingleProduct;

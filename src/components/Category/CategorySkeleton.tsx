e3c5d308651e7a309f28ece29f612b25ef749b2d
rinkuahamed <ringkuali18@gmail.com>
2023-11-16 22:55:02 +0600

    update

--- src/components/Category/CategorySkeleton.tsx
+++ src/components/Category/CategorySkeleton.tsx
@@ -2,7 +2,14 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const CategorySkeleton = () => {
-  return <Skeleton count={2} />;
+  return (
+    <div>
+      <Skeleton width={187} height={124} />
+      <p className="text-center font-semibold mt-2">
+        <Skeleton height={24} />
+      </p>
+    </div>
+  );
 };
 
 export default CategorySkeleton;
--- src/components/Product/ProductSkeleton.tsx
+++ src/components/Product/ProductSkeleton.tsx
@@ -2,7 +2,20 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const ProductSkeleton = () => {
-  return <Skeleton count={5} />;
+  return (
+    <div>
+      <Skeleton height={180} />
+      <p className="text-center font-semibold mt-2">
+        <Skeleton />
+      </p>
+      <p className="text-center font-semibold mt-2">
+        <Skeleton />
+      </p>
+      <p className="text-center font-semibold mt-2">
+        <Skeleton height={34} />
+      </p>
+    </div>
+  );
 };
 
 export default ProductSkeleton;
